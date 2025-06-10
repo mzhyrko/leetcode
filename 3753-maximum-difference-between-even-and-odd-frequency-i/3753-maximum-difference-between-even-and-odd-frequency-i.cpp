f0b68d5bffdea130012b2c945595572665783d5b
Mikhail Zhyrko <mikhail.zhirko@gmail.com>
2025-06-10 14:14:38 +0200

    3753-MAXIMUM-DIFFERENCE-BETWEEN-EVEN-AND-ODD-FREQUENCY-I Stats: Time: 3 ms (34.81%), Space: 9.4 MB (59.15%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int maxDifference(string s) {
-        vector<int> tmp(26,0);
-        vector<int> even;
-        vector<int> odd;
-        int max_diff = INT_MIN;
-        
-        for(auto a:s){
-            tmp[a-'a']++;
+        int odd=-1;
+        int even=INT_MAX;
+        vector<int> freq(26);
+        for(char c:s) 
+            freq[c-'a']++;
+        for(int i=0;i<26;i++){
+            if(freq[i]%2)
+                odd=max(odd,freq[i]);
+            else if(freq[i]) 
+                even=min(even,freq[i]);
         }
-
-        for(auto a:tmp){
-            if(a%2==1){
-                odd.push_back(a);
-            }
-            else if(a%2==0 && a != 0)
-                even.push_back(a);
-        }
-        
-        for(int i = 0; i < odd.size(); i++){
-            for(int j = 0; j < even.size(); j++){
-                if(odd[i] - even[j] >= max_diff){
-                    max_diff = odd[i] - even[j];
-                }
-            }
-        }
-
-        return max_diff;
+        return odd-even;
     }
 };
